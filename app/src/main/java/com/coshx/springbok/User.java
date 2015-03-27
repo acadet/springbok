78d634b670f38d60d4148944e20ed2deaf26885f
Adrien Cadet <acadet@live.fr>
2015-03-26 18:28:31 -0700

    Adds first test + a lint warning

---
+++ app/src/main/java/com/coshx/springbok/User.java
@@ -0,0 +1,17 @@
+package com.coshx.springbok;
+
+/**
+ * @class User
+ * @brief
+ */
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
---
+++ app/src/test/java/com/coshx/springbok/UserTest.java
@@ -0,0 +1,28 @@
+package com.coshx.springbok;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(emulateSdk = 18, reportSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class UserTest {
+
+    @Test
+    public void UserNameTest() {
+        User u = new User();
+
+        u.setName("foo");
+
+        Assert.assertEquals("foo", u.getName());
+    }
+
+    // Uncomment test below for testing failed builds
+    //    @Test
+    //    public void failingTest() {
+    //        Assert.assertFalse(true);
+    //    }
+}
