d5f18bddc7f7a2108ff5c00242e233bb26a4d383
Adrien Cadet <acadet@live.fr>
2015-04-09 10:27:40 -0400

    Adds custom test runner

---
+++ app/src/test/java/com/coshx/springbok/GradleRoboletricTestRunner.java
@@ -0,0 +1,67 @@
+package com.coshx.springbok;
+
+import android.app.Activity;
+import android.os.Build;
+
+import org.robolectric.AndroidManifest;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.res.Fs;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @class GradleRoboletricTestRunner
+ * @brief
+ */
+public class GradleRoboletricTestRunner extends RobolectricTestRunner {
+    private static final String PROJECT_DIR =
+        getProjectDirectory();
+
+    // Include manifest
+    private static final String MANIFEST_PROPERTY =
+        PROJECT_DIR + "src/main/AndroidManifest.xml";
+
+    // Include compiled resources (not the raw ones)
+    private static final String RES_PROPERTY =
+        PROJECT_DIR + "build/intermediates/res/app/debug/";
+
+    // Define SDK to use
+    private static final int MAX_SDK_SUPPORTED_BY_ROBOLECTRIC =
+        Build.VERSION_CODES.JELLY_BEAN_MR2;
+
+    public GradleRoboletricTestRunner(final Class<?> testClass) throws Exception {
+        super(testClass);
+    }
+
+    private static String getProjectDirectory() {
+        String path = "";
+        try {
+            File file = new File("..");
+            path = file.getCanonicalPath();
+            path = path + "/app/";
+        } catch (IOException ex) {
+        }
+        return path;
+    }
+
+    @Override
+    public AndroidManifest getAppManifest(Config config) {
+        return new AndroidManifest(
+            Fs.fileFromPath(MANIFEST_PROPERTY),
+            Fs.fileFromPath(RES_PROPERTY)
+        ) {
+            @Override
+            public int getTargetSdkVersion() {
+                return MAX_SDK_SUPPORTED_BY_ROBOLECTRIC;
+            }
+
+            @Override
+            public String getThemeRef(Class<? extends Activity> activityClass) {
+                return "@style/AppTheme"; // Needs to define default theme
+            }
+
+        };
+    }
+}
---
+++ app/src/test/java/com/coshx/springbok/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.coshx.springbok;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+
+/**
+ * @class MainActivityTest
+ * @brief
+ */
+@RunWith(GradleRoboletricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity _activity;
+
+    @Before
+    public void setUp() {
+        _activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void notNull() {
+        Assert.assertNotNull(_activity);
+    }
+}
